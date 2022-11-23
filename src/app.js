8c01f34a55f75625500a4508f830af8b1613399b
Carla Gonçalves <goncalves.carla1998@gmail.com>
2022-11-23 14:24:09 +0000

    workshop done

--- src/app.js
+++ src/app.js
@@ -7,6 +7,28 @@ const items = [
   { id: 2, name: 'item2' },
 ];
 
+// defining the middleware
+const middleware1 = (req, res, next) => {
+  console.log('doing stuff in middleware 1');
+  next();
+};
+
+const middleware2 = (req, res, next) => {
+  console.log('doing stuff in middleware 2');
+  next();
+};
+
+// linking the middleware to the express app
+app.get('/myroute', middleware1, middleware2, (req, res) => {
+  console.log('handling /myroute');
+  res.send('content for /myroute');
+});
+
+app.get('/hello', middleware2, (req, res) => {
+  console.log('handling /hello');
+  res.send(`Hello ${req.query.name} !`);
+});
+
 app.get('/things', (req, res) => {
   console.log('handling GET /things');
   res.send(items);
